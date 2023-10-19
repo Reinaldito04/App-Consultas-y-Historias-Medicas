fc92270e0ddd3b8b893ff9a34fe33ad77a8f08de
CersitaXD <cesarperozoetir@gmail.com>
2023-10-19 11:43:14 -0400

    POR TERMINAR

---
+++ interfaces/placasN.py
@@ -0,0 +1,284 @@
+from PyQt5 import QtCore,QtGui,QtWidgets
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import QDate , QBuffer, QByteArray , QTime
+from PyQt5.QtGui import QImage,QPixmap 
+from PyQt5.QtCore import QIODevice
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QWidget ,QApplication ,QMainWindow,QGraphicsDropShadowEffect, QCalendarWidget , QBoxLayout
+from PyQt5.QtWidgets import QMessageBox,QLabel,QTableWidgetItem
+import sqlite3
+from PyQt5.QtCore import Qt
+import os 
+
+class Ui_placas(QMainWindow):
+    def __init__(self):
+        super(Ui_placas, self).__init__()
+        loadUi("interfaces\placas(nueva).ui", self)
+        self.btn_agg.clicked.connect(self.addplacas)
+        self.btn_buscar.clicked.connect(self.searchData)
+        self.btn_clear.clicked.connect(self.clearInputs)
+        self.actionSalir.triggered.connect(self.salir)
+        self.btn_import.clicked.connect(self.addPhoto)
+
+        self.btn_buscar_2.clicked.connect(self.buscarDatos)
+        self.btn_buscar_2.clicked.connect(self.searchAll)
+        self.btn_buscar_2.clicked.connect(self.searchForDelete)
+        self.btn_delete.clicked.connect(self.DeletePlaca)
+        
+    def salir(self):
+       QApplication.quit()
+       
+    def clearInputs(self):
+        self.in_busqueda.clear()
+        self.in_busqueda_2.clear()
+        self.in_name.clear()
+        self.in_apell.clear()
+        self.in_name2.clear()
+        self.in_apell2.clear()
+        
+    def addplacas(self):
+        cedula = self.in_busqueda.text()
+        foto_pixmap1  =self.img1.pixmap()
+        foto_pixmap2  =self.img2.pixmap()
+        foto_pixmap3  =self.img3.pixmap()
+        if foto_pixmap1 is None or foto_pixmap2 is None or foto_pixmap3 is None:
+            QMessageBox.warning(self,"Advertencia","Debes importar 3 imagenes antes de guardar")
+            return
+        if len(cedula) <=0 :
+             QMessageBox.warning(self,"Advertencia","Debes ingresar la cedula para almacenar las placas")
+             return
+        else:
+            try:
+                foto1_image = foto_pixmap1.toImage()
+                foto2_image = foto_pixmap2.toImage()
+                foto3_image = foto_pixmap3.toImage()
+
+                # Convierte cada imagen a un formato de bytes (por ejemplo, PNG)
+                foto1_bytes = QByteArray()
+                buffer1 = QBuffer(foto1_bytes)
+                buffer1.open(QIODevice.WriteOnly)
+                foto1_image.save(buffer1, "PNG")
+                foto1_byte = buffer1.data()
+                buffer1.close()
+
+                foto2_bytes = QByteArray()
+                buffer2 = QBuffer(foto2_bytes)
+                buffer2.open(QIODevice.WriteOnly)
+                foto2_image.save(buffer2, "PNG")
+                foto2_byte = buffer2.data()
+                buffer2.close()
+
+                foto3_bytes = QByteArray()
+                buffer3 = QBuffer(foto3_bytes)
+                buffer3.open(QIODevice.WriteOnly)
+                foto3_image.save(buffer3, "PNG")
+                foto3_byte = buffer3.data()
+                buffer3.close()
+                conexion = sqlite3.connect('interfaces\database.db')
+                cursor = conexion.cursor()
+                cursor.execute("UPDATE Pacientes SET Placa1 = ?, Placa2 = ? ,Placa3 = ? WHERE Cedula = ?",
+                            (foto1_byte, foto2_byte, foto3_byte ,cedula ))
+                QMessageBox.information(self, "Exito", "Datos Guardados Correctamente ")
+                conexion.commit()
+                conexion.close()
+            except sqlite3.Error as e:
+                QMessageBox.critical(self, "Error", "Error al actualizar los datos en la base de datos: " + str(e))
+        
+    def searchData(self):
+        idUser = self.id_user
+        cedula =self.in_busqueda.text()
+        if len(cedula) <=0:
+            QMessageBox.warning(self,"Error","Ingrese una cedula")
+        else:
+            
+            try:
+                conexion = sqlite3.connect('interfaces\database.db')
+                cursor = conexion.cursor()
+                cursor.execute("SELECT Nombre , Apellido FROM Pacientes WHERE Cedula = ? AND ID_user = ?",(cedula, idUser))
+                resultado = cursor.fetchone()
+                conexion.close()
+                if resultado :
+                    nombre_paciente ,apellido_paciente = resultado
+                    self.in_name.setText(nombre_paciente)
+                    self.in_apell.setText(apellido_paciente)
+                else:
+                    QMessageBox.critical(self,"Error","El paciente no fue encontrado")
+            except sqlite3.Error as error:
+                QMessageBox.critical(self, "Error", f"Error al buscar paciente: {str(error)}")
+
+    def addPhoto(self):
+            filenames, _ = QFileDialog.getOpenFileNames(self, "Seleccionar imágenes", "", "Archivos de imagen (*.png *.jpg *.bmp *.jpeg)")
+                
+            if len(filenames) >= 3:
+                    pixmap1 = QPixmap(filenames[0])
+                    pixmap2 = QPixmap(filenames[1])
+                    pixmap3 = QPixmap(filenames[2])
+                        
+                    self.foto.setPixmap(pixmap2)
+                    self.foto.setPixmap(pixmap3)
+                    self.foto.setPixmap(pixmap1)
+            else:
+                    QMessageBox.information(self,"Imagenes","Por favor,Selecciona una imagen")    
+    
+    def buscarDatos(self):
+        cedula = self.in_busqueda_2.text()
+        idUser = self.id_user
+        if len(cedula)<= 0:
+         QMessageBox.warning(self,"Advertencia","Ingrese una Cédula")
+        else:
+            try:
+                conexion = sqlite3.connect('interfaces\database.db')
+                cursor =conexion.cursor()
+                cursor.execute("SELECT Nombre , Apellido , Placa1, Placa2, Placa3 FROM Pacientes WHERE Cedula = ? AND ID_user = ?",(cedula, idUser))
+                resultado = cursor.fetchone()
+                if resultado:
+                    nombre_paciente , apellido_paciente ,placa1 , placa2,placa3 = resultado
+                    self.in_name_2.setText(nombre_paciente)
+                    self.in_apell_2.setText(apellido_paciente)
+                    pixmap1 = QPixmap()
+                    pixmap1.loadFromData(placa1)
+                    self.img4.setPixmap(pixmap1)
+                    
+                    pixmap2 = QPixmap()
+                    pixmap2.loadFromData(placa2)
+                    self.img5.setPixmap(pixmap2)
+                    
+                    pixmap3 = QPixmap()
+                    pixmap3.loadFromData(placa3)
+                    self.img6.setPixmap(pixmap3)
+            except sqlite3.Error as e:
+                QMessageBox.critical(self, "Error", "Error al actualizar los datos en la base de datos: " + str(e))                   
+    def searchAll(self):
+        try:
+            conexion = sqlite3.connect('interfaces/database.db')
+            cursor = conexion.cursor()
+            idUser = self.id_user
+
+            # Ejecuta una consulta para obtener los datos de los pacientes y ordenar por Fecha_Cita descendente
+            cursor.execute("SELECT Cedula, Nombre, Apellido, Placa1 , Placa2 , Placa3   FROM Pacientes WHERE ID_user = ? ", (idUser,))
+            tabla_placas = cursor.fetchall()
+            conexion.close()
+
+            # Filtrar las filas que no contengan ningún valor 'None'
+            filas_filtradas = [paciente for paciente in tabla_placas if None not in paciente]
+
+            # Limpiar la tabla existente si es necesario
+            self.tabla_placas.clearContents()
+
+            # Establecer el número de filas y columnas en la tabla
+            self.tabla_placas.setRowCount(len(filas_filtradas))
+            self.tabla_placas.setColumnCount(len(filas_filtradas[0]))
+
+            # Agregar los datos a la tabla
+            for row, paciente in enumerate(filas_filtradas):
+                for column, value in enumerate(paciente):
+                    if column == 6:  
+                        if value == 1:
+                           
+                            item = QTableWidgetItem("Imágenes disponibles")
+                        else:
+                               item = QTableWidgetItem("No hay imágenes")
+                    elif column in [3, 4, 5]:  # Supongamos que las columnas 3, 4 y 5 contienen las rutas de las imágenes
+                        if value is not None and os.path.exists(value):                           
+                            pixmap = QPixmap(value)
+                            if not pixmap.isNull():
+                                item = QTableWidgetItem()
+                                item.setData(Qt.DecorationRole, pixmap)
+                            else:
+                               
+                                item = QTableWidgetItem("Imagen dañada")
+                        else:
+                        
+                            item = QTableWidgetItem("Imagen faltante")
+                    else:
+                        item = QTableWidgetItem(str(value))
+                    self.tabla_placas.setItem(row, column, item)
+        except:
+            QMessageBox.critical(self, "Error", "No hay pacientes con placas actualmente.")
+            
+    def searchForDelete(self):
+        try:
+            conexion = sqlite3.connect('interfaces/database.db')
+            cursor = conexion.cursor()
+            idUser = self.id_user
+            cedula = self.in_busqueda_3.text()
+            if len(cedula)<= 0:
+                QMessageBox.critical(self,"Error","Ingrese la cedula primeramente")
+                return
+            else:
+                
+                # Ejecuta una consulta para obtener los datos de los pacientes y ordenar por Fecha_Cita descendente
+                cursor.execute("SELECT Cedula, Nombre, Apellido, Placa1 , Placa2 , Placa3   FROM Pacientes WHERE ID_user = ? AND Cedula = ? ", (idUser,cedula ))
+                tabla_placas = cursor.fetchall()
+                conexion.close()
+
+                # Filtrar las filas que no contengan ningún valor 'None'
+                filas_filtradas = [paciente for paciente in tabla_placas if None not in paciente]
+
+                # Limpiar la tabla existente si es necesario
+                self.tabla_delete.clearContents()
+
+                # Establecer el número de filas y columnas en la tabla
+                self.tabla_delete.setRowCount(len(filas_filtradas))
+                self.tabla_delete.setColumnCount(len(filas_filtradas[0]))
+
+                # Agregar los datos a la tabla
+                for row, paciente in enumerate(filas_filtradas):
+                    for column, value in enumerate(paciente):
+                        if column == 6:  # Supongamos que la columna 6 es "ImagenesDisponibles"
+                            if value == 1:
+                                # El paciente tiene imágenes, muestra un mensaje o icono personalizado
+                                item = QTableWidgetItem("Imágenes disponibles")
+                            else:
+                                # El paciente no tiene imágenes, muestra un mensaje o icono personalizado
+                                item = QTableWidgetItem("No hay imágenes")
+                        elif column in [3, 4, 5]:  # Supongamos que las columnas 3, 4 y 5 contienen las rutas de las imágenes
+                            if value is not None and os.path.exists(value):
+                                pixmap = QPixmap(value)
+                                if not pixmap.isNull():
+                                    item = QTableWidgetItem()
+                                    item.setData(Qt.DecorationRole, pixmap)
+                                else:
+                                    # La imagen está dañada
+                                    # Mostrar un ícono de error en lugar de la imagen
+                                    item = QTableWidgetItem("Imagen dañada")
+                            else:
+                                # La imagen está ausente
+                                # Mostrar un ícono de imagen faltante en lugar de la imagen
+                                item = QTableWidgetItem("Imagen faltante")
+                        else:
+                            item = QTableWidgetItem(str(value))
+                        self.tabla_delete.setItem(row, column, item)
+        except:
+            QMessageBox.critical(self, "Error", "El paciente no tiene placas actualmente.")
+            
+    def DeletePlaca(self):
+        try:
+            cedula = self.in_busqueda_3.text()
+    
+            if len(cedula) == 0:
+                QMessageBox.critical(self, "Error", "Ingrese una cédula")
+            else:
+                Placa1 = None
+                Placa2 = None
+                Placa3 = None
+                conexion = sqlite3.connect('interfaces/database.db')
+                cursor = conexion.cursor()
+                cursor.execute("UPDATE Pacientes SET Placa1 = ?, Placa2 = ? , Placa3= ? WHERE Cedula = ?",
+                        (Placa1, Placa2, Placa3 , cedula))
+                conexion.commit()
+                conexion.close()
+        
+        # Eliminación exitosa, muestra un mensaje y realiza otras acciones si es necesario
+                QMessageBox.information(self, "Realizado", "Las placas ha sido eliminada correctamente")
+                self.tabla_delete.clearContents()
+                self.in_busqueda_3.clear()
+        except sqlite3.Error as e:
+            QMessageBox.critical(self, "Error", "Error al eliminar las placas  de la base de datos: " + str(e))
+        pass
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    placa= Ui_placas()
+    placa.show()
+    sys.exit(app.exec_())
