a8235cdf4363b050ce012d452801ae9fab4ea34f
Reinaldo <reybellorin82@gmail.com>
2023-11-08 19:14:48 -0400

    Cambios en filtros de busqueda de pacientes

--- interfaces/paciente_view.py
+++ interfaces/paciente_view.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox,QLabel,QTableWidgetItem
 from PyQt5.uic import loadUi
 import sqlite3
 
@@ -10,52 +11,54 @@ class Ui_pacientes_view(QtWidgets.QMainWindow):
         self.actionSalir.triggered.connect(self.salir)
         self.bt_act.clicked.connect(self.act_T)
         self.bt_buscar.clicked.connect(self.buscar)
-
+        self.cargarPacientes()
         self.filtro = self.findChild(QtWidgets.QComboBox, "filtro")
         self.filtro.addItem("Seleccione una opción para filtrar")
-        self.filtro.addItems(["Cedula", "Nombre", "Edad", "Genero", "Fecha"])
-
+        self.filtro.addItems(["Dentista","Cedula", "Nombre", "Edad", "Sexo","Direccion" , "Fecha_Diagnotico"])
+        
     def act_T(self):
         self.cargarPacientes()
 
-    def cargarPacientes(self):
-        model = QtGui.QStandardItemModel()
-        self.tabla_p.setModel(model)
-        headers = ["Cedula", "Nombre", "Apellido", "Edad", "Dirección", "Sexo", "Fecha"]
-        model.setColumnCount(len(headers))
-        model.setHorizontalHeaderLabels(headers)
+    def cargarPacientes(self, filtro=None, valor=None):
+        self.tabla_p.setRowCount(0)  # Limpiar la tabla actual
+        headers = ["Cedula", "Nombre", "Apellido", "Edad", "Dirección", "Sexo", "Fecha_Diagnotico"]
 
         try:
             conexion = sqlite3.connect('interfaces/database.db')
             cursor = conexion.cursor()
 
-            # Obtén el ID del usuario actual
-            id_User = self.id_User  # Asegúrate de que esta variable esté definida y tenga el valor correcto
+            if filtro and valor:
+                cursor.execute("SELECT Cedula, Nombre, Apellido, Edad, Direccion, Sexo, Fecha_Diagnotico FROM Pacientes WHERE {} LIKE ? ORDER BY Fecha_Diagnotico ASC".format(filtro), ('%' + valor + '%',))
+            else:
+                cursor.execute("SELECT Cedula, Nombre, Apellido, Edad, Direccion, Sexo, Fecha_Diagnotico FROM Pacientes ORDER BY Fecha_Diagnotico ASC")
 
-            # Ejecuta una consulta para obtener los datos de los pacientes y ordenar por Fecha_Cita descendente
-            cursor.execute("SELECT Cedula, Nombre, Apellido, Edad, Direccion, Sexo, Fecha_Cita FROM Pacientes WHERE ID_user = ? ORDER BY Fecha_Cita ASC", (id_User,))
             pacientes = cursor.fetchall()
 
+            self.tabla_p.setColumnCount(len(headers))
+            self.tabla_p.setHorizontalHeaderLabels(headers)
+
             for row, paciente in enumerate(pacientes):
+                self.tabla_p.insertRow(row)
                 for column, value in enumerate(paciente):
-                    item = QtGui.QStandardItem(str(value))
-                    model.setItem(row, column, item)
+                    item = QtWidgets.QTableWidgetItem(str(value))
+                    self.tabla_p.setItem(row, column, item)
 
             conexion.close()
         except sqlite3.Error as e:
             QtWidgets.QMessageBox.critical(self, "Error", "Error al consultar la base de datos: " + str(e))
-
+        
     def buscar(self):
-        if self.filtro.currentIndex() == 0:
+        filtro = self.filtro.currentText()
+        valor = self.in_buscar.text()
+        if filtro == "Seleccione una opción para filtrar":
             QtWidgets.QMessageBox.warning(self, "Error", "Debe seleccionar un filtro para buscar")
+        elif not valor:
+            QtWidgets.QMessageBox.warning(self, "Por favor", "Ingrese alguna especificación del paciente para realizar la búsqueda")
+        elif self.tabla_p.rowCount() == 0:
+            QtWidgets.QMessageBox.warning(self, "Advertencia", "No se ha encontrado ningún registro")  
         else:
-            filtro = self.filtro.currentText()
-            valor = self.in_buscar.text()
-            self.buscarPacientes(filtro, valor)
-
-    def buscarPacientes(self, filtro, valor):
-        print("si")
-
+            self.cargarPacientes(filtro, valor)
+        
     def salir(self):
         reply = QtWidgets.QMessageBox.question(
             self, 'Confirmación', '¿Desea Salir?',
@@ -66,7 +69,7 @@ class Ui_pacientes_view(QtWidgets.QMainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    app.setApplicationName("si")  # Establecer el nombre de la aplicación
+    app.setApplicationName("si") 
 
     Vista_P = Ui_pacientes_view()
     Vista_P.showFullScreen()
