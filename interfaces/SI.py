48064917fcff7d079cea9d9e530015a201bddf42
CersitaXD <cesarperozoetir@gmail.com>
2023-11-13 12:39:17 -0400

    Seccion de montos para los tratamientos, funcional

--- interfaces/SI.py
+++ interfaces/SI.py
@@ -10,7 +10,7 @@ class Ui_SI(QtWidgets.QMainWindow):
         self.setWindowTitle("SI")
 
         self.tratamientos = {
-            "Triaje": ["Consulta e Historia Clínica sin informe", "Consulta e Historia Clínica con informe"],
+            "Triaje": ["Seleccione el tipo de honorario", "Consulta e Historia Clínica sin informe", "Consulta e Historia Clínica con informe"],
             "Periodoncia": ["Tartectomía y pulido simple (1 sesión)", "Tartectomía y pulido simple (2-3 sesiones)","Aplicación tópica de fluór","Cirguia periodontal (por cuadrante)"],
             "Blanqueamiento": ["Blanqueamiento intrapulpar", "Blanquemaineto maxilar superior e inferior (2 sesiones de 20 min c/u)"],
             "Operatoria": ["Obturaciones provisionales","Obturaciones con Amalgama","Obturaciones con vidrio ionomerico pequeña","Obturaciones con vidrio ionomerico grande","Obturaciones con resina fotocurada"],
@@ -21,40 +21,22 @@ class Ui_SI(QtWidgets.QMainWindow):
             "Protesis removibles metalicas y/o acrilicas": ["1 a 3 unidades","4 a 6 unidades","7 a 12 unidades","Unidadad adicional","Ganchos contorneados retentativas acrilicas c/u","Reparaciones protesis acrilicas y/oo agregar un diente a la protesis"],
             "Protesis totales": ["Dentadura superior o inferior (incluye controles post-inatalción) c/u"],
             "Implantes dentales": ["Honorarios cirujano por implante","Implante y aditamientos","Injertos óseos (1cc)","PRF (incluye bionalista y extraccion de sangre + centrifugado)","Corona metal porcelana sobre implante","DPR acrilica"],
-       
         }
 
-        self.c_0 = self.findChild(QtWidgets.QComboBox, "c_0")
-        self.c_1 = self.findChild(QtWidgets.QComboBox, "c_1")
-        self.c_2 = self.findChild(QtWidgets.QComboBox, "c_2")
-        self.c_3 = self.findChild(QtWidgets.QComboBox, "c_3")
-        self.c_4 = self.findChild(QtWidgets.QComboBox, "c_4")
-        self.c_5 = self.findChild(QtWidgets.QComboBox, "c_5")
+        self.combo_honorario = [self.findChild(QtWidgets.QComboBox, f"c_{i}") for i in range(6)]
+        self.combo_tratamiento = [self.findChild(QtWidgets.QComboBox, f"t_{i}") for i in range(6)]
+        self.monto_dola = [self.findChild(QtWidgets.QLineEdit, f"monto_dola_{i+1}") for i in range(6)]
 
-        self.t_0 = self.findChild(QtWidgets.QComboBox, "t_0")
-        self.t_1 = self.findChild(QtWidgets.QComboBox, "t_1")
-        self.t_2 = self.findChild(QtWidgets.QComboBox, "t_2")
-        self.t_3 = self.findChild(QtWidgets.QComboBox, "t_3")
-        self.t_4 = self.findChild(QtWidgets.QComboBox, "t_4")
-        self.t_5 = self.findChild(QtWidgets.QComboBox, "t_5")
-
-        self.combo_pairs = {
-            self.c_0: self.t_0,
-            self.c_1: self.t_1,
-            self.c_2: self.t_2,
-            self.c_3: self.t_3,
-            self.c_4: self.t_4,
-            self.c_5: self.t_5,
-        }
-
-        for combo in self.combo_pairs:
+        for combo in self.combo_honorario:
             combo.addItem("Seleccione el tipo de honorario")
             combo.addItems(list(self.tratamientos.keys()))
             combo.currentTextChanged.connect(self.loadTratamientos)
 
+        for i, combo in enumerate(self.combo_tratamiento):
+            combo.currentTextChanged.connect(lambda _, index=i: self.update_monto(index))
+
     def loadTratamientos(self):
         sender = self.sender()
-
         selected_honorario = sender.currentText()
 
         if selected_honorario == "Seleccione el tipo de honorario":
@@ -63,17 +45,31 @@ class Ui_SI(QtWidgets.QMainWindow):
         conexion = sqlite3.connect('interfaces/database.db')
         cursor = conexion.cursor()
 
-        linked_combo = self.combo_pairs[sender]
-        linked_combo.clear()
+        index = self.combo_honorario.index(sender)
 
         tratamientos_honorario = self.tratamientos.get(selected_honorario, [])
+        linked_combo = self.combo_tratamiento[index]
+        linked_combo.clear()
 
         for tratamiento in tratamientos_honorario:
             cursor.execute("SELECT monto FROM Trata WHERE tipo_tratamiento = ? AND tratamiento = ?", (selected_honorario, tratamiento))
             monto = cursor.fetchone()
             if monto:
                 linked_combo.addItem(tratamiento)
-                
+
+    def update_monto(self, index):
+        selected_honorario = self.combo_honorario[index].currentText()
+        selected_tratamiento = self.combo_tratamiento[index].currentText()
+
+        conexion = sqlite3.connect('interfaces/database.db')
+        cursor = conexion.cursor()
+
+        cursor.execute("SELECT monto FROM Trata WHERE tipo_tratamiento = ? AND tratamiento = ?", (selected_honorario, selected_tratamiento))
+        monto = cursor.fetchone()
+
+        if monto:
+            self.monto_dola[index].setText(str(monto[0]))
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     app.setApplicationName("si")
